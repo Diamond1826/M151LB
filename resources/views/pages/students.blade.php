c0ec186a40df88f94bbaac5d6c9c931ef1c30c42
Diamond1826 <diamond1826@hotmail.com>
2017-07-03 17:36:12 +0200

    Alpha1.4

--- resources/views/pages/students.blade.php
+++ resources/views/pages/students.blade.php
@@ -5,10 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Student's List</div>
 
                 <div class="panel-body">
-                    You are logged in!<br>
                     <div class="table-responsive">
                         <table class="table">
                             <tr>
