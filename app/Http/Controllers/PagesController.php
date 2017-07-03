4e3db7f6ce4ad479cd8bed8d0f4a2b1ffaec4f53
Diamond1826 <diamond1826@hotmail.com>
2017-07-03 14:54:22 +0200

    Alpha1

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    /*public function __construct()
+    {
+        $this->middleware('auth');
+    }*/
+
+    public function index() 
+    {
+        return view('pages.index');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+
+    public function login() 
+    {
+        return view('auth.login');
+    }
+
+    public function registry() 
+    {
+        return view('auth.register');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','PagesController@index');
+
+Route::get('/home', 'PagesController@index');
+
+Route::get('/contact', 'PagesController@contact');
+
+Route::get('/registry','PagesController@registry');
+
+Route::get('/login', 'PagesController@login');
+
+Auth::routes();
+
+Route::get('/logout', function (
+{
+	Auth::logout();
+	return redirect('/');
+});
+
+Route::auth();
