0ece04e2c0a27d9b57b76fef91f5387a2126f1f5
Diamond1826 <diamond1826@hotmail.com>
2017-07-06 18:34:09 +0200

    makeup code

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to Students Overview</div>
+                <div class="panel-heading">Welcome to Student's Overview</div>
 
                 <div class="panel-body">
                     <span id="textLoggedIn">You are logged in!</span>
@@ -20,7 +20,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to Students Overview</div>
+                <div class="panel-heading">Welcome to Student's Overview</div>
 
                 <div class="panel-body">
                     Login to use it!
